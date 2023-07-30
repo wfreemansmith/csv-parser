9065b35fb86019fcb27ed9276b260fac1d31366c
Wes <wfreemansmith@yahoo.co.uk>
2023-07-30 16:50:24 +0100

    initial commit

---
+++ csv-parser.js
@@ -0,0 +1,62 @@
+const fs = require("fs/promises");
+const space = /[\s+-]/g;
+const currency = /[£$€]/g;
+const carriageReturn = /\r/g;
+
+async function read(fileName) {
+  // reads contents of CSV file and returns an array representing each line
+  // removes carriage returns
+  // removes currency symbols
+
+  try {
+    const csv = await fs.readFile(fileName, "utf-8");
+    return csv.replace(carriageReturn, "").replace(currency, "").split("\n");
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function createTable(fileName, tableName) {
+  // Will read CSV file and create table based on data types
+  // If no table name is specified, will create it based on csv name
+  // Names have underscores rather than spaces and are all lowerCase
+  if (!tableName) tableName = fileName.split(".")[0];
+  tableName = tableName.replace(space, "_").toLowerCase();
+}
+
+async function insertCSV(fileName, tableName) {
+  // reads a CSV file and converts it into an "INSERT INTO" SQL command
+  // takes 2 arguments: the name of the table to insert into, and the name or path of the csv file
+  // removes spaces from column names
+  // add another feature: if no table, create table
+
+  try {
+    const csv = await read(fileName);
+
+    const columns = csv[0]
+      .split(",")
+      .map((column) => column.replace(space, "_").toLowerCase())
+      .join(", ");
+
+    const rows = csv.slice(1).map((row) => {
+      return row
+        .split(",")
+        .map((item) => `'${item}'`)
+        .join(", ");
+    });
+
+    let SQL = `INSERT INTO ${tableName} (${columns})\nVALUES \n`;
+
+    rows.forEach((row, i) => {
+      SQL += `(${row})${i === rows.length - 1 ? `;` : `,\n`}`;
+    });
+
+    console.log(SQL);
+    return SQL;
+  } catch (err) {
+    console.log("Error:", err);
+  }
+}
+
+// createTable("wedsheet.csv");
+// insertCSV("wedsheet.csv", "wedsheet")
